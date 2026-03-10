bcff6ce8e41f53caada8c6dae0b716146a471660
saikiran-221 <ramakrishnamiriyala1977@gmail.com>
2026-03-10 10:00:11 +0530

    Fix: redirect root to feed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { CreatePost } from './pages/CreatePost';
 import { Feed } from './pages/Feed';
 import './App.css'
@@ -9,6 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Navigate to='/feed' replace />} />
           <Route path='/create-post' element={<CreatePost />} />
           <Route path='/feed' element={<Feed />} />
         </Routes>
